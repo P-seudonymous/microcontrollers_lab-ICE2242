ff692e196acebbf0f5144ac5b2457ad1c49c3683
P-seudonymous <aryan.sarafhp@gmail.com>
2026-04-22 03:26:09 +0530

    q1_lab9

---
+++ lab9/q1.c
@@ -0,0 +1,27 @@
+//90degrees in clockwise
+#include <lpc21xx.h>
+
+void cw();
+unsigned int i,j,k;
+unsigned long int var1, var2;
+
+int main(){
+    PINSEL0 = 0x00FFFFFF;
+    IO0DIR |= 0x0000F000; 
+    while(1){
+        for(j=0;j<12;j++)
+            cw();
+        for(k=0;k<6500000;k++);
+    }
+}
+
+
+void cw(){
+    var1 = 0x00000800;
+    for(i=0;i<3;i++){
+    var1 = var1 << 1;
+    var2 = ~var1;
+    var2 = var2 & 0x0000F000;
+    for(k=0;k<5000;k++);
+    }
+}
