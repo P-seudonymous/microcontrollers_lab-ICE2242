1c9c0094bdd10c4ad017e0dabb8493b484db7d3e
P-seudonymous <aryan.sarafhp@gmail.com>
2026-04-22 02:39:32 +0530

    q3_lab8

---
+++ lab8/q2.c
@@ -0,0 +1,23 @@
+// anti clockwise for  a given period of time
+#include <lpc21xx.h>
+
+void cw();
+unsigned int i;
+
+int main(){
+    IO0DIR = 0x00000900;
+    IO0SET = 0x00000100;
+    while(1){
+        a_cw();
+        for(i=0;i<500000;i++)
+    }
+}
+
+void a_cw(){
+    IO0CLR = 0x00000100;
+    for(i=0;i<100000;i++);
+    IO0SET = 0x00000900;
+    for(i=0;i<100000;i++);
+    IO0CLR = 0x00000100;}
+}
+
