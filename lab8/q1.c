09bb25d182516cfc59757a44d8835d90c9a97ec4
P-seudonymous <aryan.sarafhp@gmail.com>
2026-04-22 02:36:34 +0530

    q1_lab8

---
+++ lab8/q1.c
@@ -0,0 +1,22 @@
+// clockwise for  a given period of time
+#include <lpc21xx.h>
+
+void cw();
+unsigned int i;
+
+int main(){
+    IO0DIR = 0x00000900;
+    IO0SET = 0x00000100;
+    while(1){
+        cw();
+        for(i=0;i<500000;i++)
+    }
+}
+
+void cw(){
+    IO0CLR = 0x00000900;
+    for(i=0;i<100000;i++);
+    IO0SET = 0x00000100;
+    for(i=0;i<100000;i++);
+    IO0CLR = 0x00000900;}
+}
